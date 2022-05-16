c6ffb85f794507c5948f4da4f308af4b1db4532d
superfishal <97133716+superfishal@users.noreply.github.com>
2022-05-16 12:38:41 -0700

    MVC file/folder structure

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+// Import modules
+const router = require("express").Router();
+// const userRoutes = require("./user-routes");
+
+// Routes
+// router.use("/users", userRoutes);
+
+module.exports = router;
---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+// const homeRoute = require("./home-route");
+// const apiRoutes = require("./api");
+
+// router.use("/", homeRoute);
+
+// router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+  res.send("<h1>Wrong Route!</h1>");
+});
+
+module.exports = router;

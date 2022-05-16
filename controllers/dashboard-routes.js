92c31f445b657b3d5006cea4ea087399ee3b0891
superfishal <97133716+superfishal@users.noreply.github.com>
2022-05-16 14:46:15 -0700

    dashboard completed, finish routes and handlebars

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,38 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
-const withAuth = require("../utils/auth");
+const withAuth = require("../utils/withAuth.js");
+router.get("/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "body", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "user_id", "post_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      // serialize data before passing to template
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const homeRoutes = require("./home-routes");
 const dashboardRoutes = require("./dashboard-routes");
 const apiRoutes = require("./api");
 
-router.use("/", homeRoute);
+router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
 

9f0ce6fe9a4e1880bf8407b3d71e3157f1ec4f59
superfishal <97133716+superfishal@users.noreply.github.com>
2022-05-27 21:04:12 -0700

    finish handlebars and routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,5 +4,12 @@ const { Post, User, Comment } = require("../models");
 // router.get("/", (req, res) => {
 //     console.log("======================");
 // })
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
+  res.render("login");
+});
 module.exports = router;

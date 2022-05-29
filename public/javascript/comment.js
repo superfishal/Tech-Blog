e24f095f42dbae502d8a84cf3882d0c55bc88df8
superfishal <97133716+superfishal@users.noreply.github.com>
2022-05-29 12:51:07 -0700

    need to finish edit/delete post

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,13 +1,12 @@
 async function commentFormHandler(event) {
   event.preventDefault();
-
+  console.log(window.location);
   const comment_text = document
     .querySelector('textarea[name="comment-body"]')
     .value.trim();
   const post_id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
-
   if (comment_text) {
     const response = await fetch("/api/comments", {
       method: "POST",
